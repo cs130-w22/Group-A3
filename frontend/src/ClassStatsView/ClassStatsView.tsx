d3992b0e47051143cc6bd25251b9b2e61d762f1f
Cindy Zheng <cindyzheng@wifi-131-179-44-192.host.ucla.edu>
2022-01-30 12:27:15 -0800

    Adding class views for professor

---
+++ frontend/src/ClassStatsView/ClassStatsView.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+import Container from "react-bootstrap/Container";
+import Stack from "react-bootstrap/Stack";
+
+const Assignments = ["Assignment 1", "Assignment 2", "Assignment 3"];
+
+//Student view of the assignment 
+const ClassStatsView = () => {
+  return (
+    <Container>
+      <h1>Class 1</h1>
+    </Container>
+  );
+};
+
+export default ClassStatsView;
