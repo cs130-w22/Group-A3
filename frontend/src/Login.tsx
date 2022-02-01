0a9a7309892bdaf769461d517d0f82d09d4ba07c
krashanoff <leo@krashanoff.com>
2022-02-01 15:40:58 -0800

    Add CI for frontend. * Enforce code style with prettier and ESLint. * Add pre-commit hooks for frontend work. * Reformat all files to meet prescribed style.

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -6,26 +6,32 @@ import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 import Stack from "react-bootstrap/Stack";
 
-const Login = () => {
+function Login() {
   const [error, setError] = useState("");
   const nav = useNavigate();
 
-  const submit: React.FormEventHandler<HTMLFormElement> = (e: React.FormEvent<HTMLFormElement>) => {
+  const submit: React.FormEventHandler<HTMLFormElement> = (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
     e.preventDefault();
-    setError(err => err ? "" : `Username is invalid.`);
-    nav('/class');
+    setError((err) => (err ? "" : `Username is invalid.`));
+    nav("/class");
   };
 
   return (
     <Container>
       <Stack direction="vertical" gap={3}>
-        {error && <Alert variant={"danger"}>Failed to login: {error}</Alert>}
+        {error && <Alert variant="danger">Failed to login: {error}</Alert>}
         <h1>Your class name</h1>
         <p>Log in to the [YOUR CLASS] grading system.</p>
         <Form onSubmit={submit}>
           <Form.Group className="mb-3" controlId="formUsername">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="text" name="username" placeholder="Josie Bruin" />
+            <Form.Control
+              type="text"
+              name="username"
+              placeholder="Josie Bruin"
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formPassword">
             <Form.Label>Password</Form.Label>
@@ -38,6 +44,6 @@ const Login = () => {
       </Stack>
     </Container>
   );
-};
+}
 
 export default Login;
