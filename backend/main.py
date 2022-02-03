1273353deaccd7dc61f41f246cd6b42657555e13
krashanoff <leo@krashanoff.com>
2022-02-03 14:44:28 -0800

    Add expiry, NBF fields to JWT. Implement login endpoint.

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@
 
 from argparse import ArgumentParser
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
 import jwt
 from flask import Flask, g, request
@@ -23,19 +23,36 @@ def get_auth() -> dict:
     Get the JSON map encoded in the request's "Authorization" header.
     Expiration is automatically checked by PyJWT. If the signature is
     expired, an ExpiredSignatureError is thrown.
+
+    The "nbf" and "exp" claims are required for each token. If they are
+    missing, then this function will throw an error.
     """
     return jwt.decode(
-        request.headers.get("Authorization"), SECRET, algorithms=["HS256"]
+        request.headers.get("Authorization"),
+        SECRET,
+        algorithms=["HS256"],
+        options={"require": ["exp", "nbf"]},
     )
 
 
-def to_auth(map: dict) -> str:
+def to_auth(payload: dict) -> str:
     """
-    Convert the provided map into a Base64-encoded JWT.
+    Convert the provided dictionary payload into a Base64-encoded JWT.
 
-    TODO: expiry renewal
+    Tokens expire in 31 days, and cannot be used before the time they
+    were issued at.
     """
-    return jwt.encode(map, SECRET, algorithm="HS256").decode("utf-8")
+    return jwt.encode(
+        {
+            **payload,
+            # Tokens are valid for 31 days.
+            "exp": datetime.now(tz=timezone.utc) + timedelta(days=31),
+            # Tokens should not be accepted before the present day.
+            "nbf": datetime.now(tz=timezone.utc),
+        },
+        SECRET,
+        algorithm="HS256",
+    ).decode("utf-8")
 
 
 @dataclass
@@ -66,17 +83,35 @@ def teardown_db(exception):
 # Log an user into the database, then return a valid JWT for their session.
 @app.route("/login", methods=["POST"])
 def login():
-    form = request.form
+    json = request.json
+    username, password = json["username"], json["password"]
 
-    return {"token": "example"}
+    conn = get_db()
+    with conn.cursor() as cur:
+        cur.execute(
+            "SELECT * FROM Accounts WHERE username = %s AND password = crypt(%s, password)",
+            [username, password],
+        )
+        records = cur.fetchall()
+        if len(records) != 1:
+            return {}, 400
+        account = records[0]
+
+    return {
+        "token": to_auth(
+            # Right now, we just store the user's ID in the token.
+            {
+                "userid": account[0],
+            }
+        ),
+    }, 200
 
 
 # Create a user in the database, then return a valid JWT for their session.
 @app.route("/user", methods=["POST"])
 def create():
-    form = request.json
-    account_type, username, password = form["type"], form["username"], form["password"]
-    # invite_key = None if account_type == "student" else form["inviteKey"]
+    json = request.json
+    account_type, username, password = json["type"], json["username"], json["password"]
 
     # Bad request
     if account_type not in ["student", "professor"]:
@@ -85,8 +120,7 @@ def create():
     # Create a database transaction to insert our accout into the associated
     # course.
     conn = get_db()
-    with conn.cursor() as cur:
-        cur.row_factory = class_row(Account)
+    with conn.cursor(row_factory=class_row(Account)) as cur:
         try:
             cur.execute(
                 "INSERT INTO Accounts (username, password, professor) VALUES (%s, %s, %s)",
@@ -132,53 +166,6 @@ def get_assignment(class_id, assignment_id):
     }, 200
 
 
-@app.route("/<class_id>/<assignment_id>/script", methods=["GET", "POST"])
-def upload_grading_script(class_id, assignment_id):
-    """
-    Upload the grading script for a specific assignment.
-    """
-    return {}, 204
-
-
-@app.route("/<class_id>/<assignment_id>/upload", methods=["GET", "POST"])
-def upload_submission(class_id, assignment_id):
-    """
-    Upload the a submission for a specific assignment.
-    """
-    return {}, 204
-
-
-@app.route("/class/<class_id>/info", methods=["GET"])
-def view_class(class_id):
-    """
-    Get all relevant information about a class, including its assignments, member list
-    (if allowed), and owner.
-    """
-
-    is_professor = True
-
-    return {
-        "name": "mycoolclass",
-        "ownerName": "Prof. Eggert",
-        "assignments": [],
-        "members": [{"name": "Svetly"}, {"name": "Preetha"}, {"name": "Leo"}]
-        if is_professor
-        else None,
-    }, 200
-
-
-@app.route("/class/<class_id>/<assignment_id>", methods=["GET"])
-def get_assignment(class_id, assignment_id):
-    """
-    Get information about an assignment for a specific user.
-    """
-    return {
-        "name": "Cool assignment one",
-        "dueDate": "1647205512354",
-        "submissions": [{"date": "1643663222161", "pointsEarned": 100.0}],
-    }, 200
-
-
 @app.route("/<class_id>/<assignment_id>/script", methods=["POST"])
 def upload_grading_script(class_id, assignment_id):
     """
@@ -191,22 +178,6 @@ def upload_grading_script(class_id, assignment_id):
 def upload_submission(class_id, assignment_id):
     """
     Upload a submission for a specific assignment.
-    """
-    return {}, 204
-
-
-@app.route("/class/<class_id>/invite", methods=["POST"])
-def create_invite(class_id):
-    """
-    Create an invite code for the class with ID `class_id`.
-    """
-    return {"inviteCode": "my-new-invite-code"}, 201
-
-
-@app.route("/class/join", methods=["POST"])
-def join_class(class_id):
-    """
-    Join the currently logged-in user to the class with ID `class-id`.
     """
     return {}, 204
 
