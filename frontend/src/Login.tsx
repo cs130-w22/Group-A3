de0cacc5e52d70c515f6efadf1f401cc02f9ed21
Cindy Zheng <41048616+cindyrzheng@users.noreply.github.com>
2022-02-16 13:18:28 -0800

    Cindyrzheng/addcontext (#49)
    * random stuff need fixing
    
    Co-authored-by: Jonathan Davies <1strangequark@users.noreply.github.com>
    
    * random backend login stuff
    
    Co-authored-by: Jonathan Davies <1strangequark@users.noreply.github.com>
    
    * Adding a context (unused still) and also adding login cookies
    
    * remove react-cookies
    
    * remove package lock file
    
    * Update frontend/src/Login.tsx
    
    Co-authored-by: leo <leo@krashanoff.com>
    
    * formatting
    
    Co-authored-by: Cindy Zheng <cindyzheng@Cindys-MacBook-Pro.local>
    Co-authored-by: leo <leo@krashanoff.com>

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,22 +1,72 @@
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 import Stack from "react-bootstrap/Stack";
+import Row from "react-bootstrap/Row";
 
+import { userContext } from "./Context/UserContext";
+
+import { useCookies } from "react-cookie";
 function Login() {
   const [error, setError] = useState("");
+  const [cookies, setCookies] = useCookies(["jwt"]);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [user, setUser] = useState({ user: { token: "" } });
+  const [token, setToken] = useState("");
+
   const nav = useNavigate();
 
-  const submit: React.FormEventHandler<HTMLFormElement> = (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
+  async function handleLogin(username: string, password: string) {
+    return fetch("http://localhost:8080/login", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        if (response.status == 401) throw "Unauthorized";
+        return response.json();
+      })
+      .then((response) => {
+        setCookies("jwt", response.token);
+      })
+      .catch((e) => {
+        setError(
+          `Failed uploading! Server responded with: ${String(e).replace(
+            "TypeError: ",
+            ""
+          )}`
+        );
+      });
+  }
+
+  async function submit(e: FormEvent) {
     e.preventDefault();
-    setError((err) => (err ? "" : `Username is invalid.`));
-    nav("/class");
-  };
+    const response = await handleLogin(username, password);
+    setUser({ user: { token: token } });
+  }
+
+  const onInputUsername = ({
+    target: { value },
+  }: {
+    target: { value: string };
+  }) => setUsername(value);
+  const onInputPassword = ({
+    target: { value },
+  }: {
+    target: { value: string };
+  }) => setPassword(value);
 
   const createAccount = () => {
     nav("/create");
@@ -49,35 +99,56 @@ function Login() {
               type="text"
               name="username"
               placeholder="Josie Bruin"
+              value={username}
+              onChange={onInputUsername}
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" name="password" />
+            <Form.Control
+              type="password"
+              name="password"
+              value={password}
+              onChange={onInputPassword}
+            />
           </Form.Group>
-          <Button variant="primary" type="submit" style={{ borderRadius: 20 }}>
-            Login
-          </Button>
-          <br />
-          <br />
-          <Button
-            onClick={forgotPassword}
-            variant="secondary"
-            type="button"
-            style={{ borderRadius: 20 }}
-          >
-            Forgot Password
-          </Button>
-          <br />
-          <br />
-          <Button
-            onClick={createAccount}
-            variant="secondary"
-            type="button"
-            style={{ borderRadius: 20 }}
-          >
-            Register
-          </Button>
+
+          <Stack direction="vertical" gap={3}>
+            <Row>
+              <Button
+                variant="primary"
+                size="lg"
+                type="submit"
+                style={{ borderRadius: 20 }}
+              >
+                Login
+              </Button>
+            </Row>
+            <Stack direction="horizontal" gap={3}>
+              <div className="me-auto">
+                <Button
+                  onClick={forgotPassword}
+                  variant="secondary"
+                  size="lg"
+                  type="button"
+                  style={{ borderRadius: 20 }}
+                >
+                  Forgot Password
+                </Button>
+              </div>
+              <div className="ms-auto">
+                <Button
+                  onClick={createAccount}
+                  variant="secondary"
+                  size="lg"
+                  type="button"
+                  style={{ borderRadius: 20 }}
+                >
+                  Register
+                </Button>
+              </div>
+            </Stack>
+          </Stack>
         </Form>
       </Stack>
     </Container>
