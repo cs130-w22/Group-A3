213eae3d20603664ce9d83928c2b7ccaa495140d
Cindy Zheng <cindyzheng@Cindys-MacBook-Pro.local>
2022-02-24 12:07:00 -0800

    Adding drop api call (still not working since class id and student id are incorrect constants

--- frontend/src/ClassListView/ClassListView.tsx
+++ frontend/src/ClassListView/ClassListView.tsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
-
+import Modal from "react-bootstrap/Modal";
+import Alert from "react-bootstrap/Alert";
+const ClassId = "1";
 const Students = [
-  { name: "Edward", grade: 90 },
-  { name: "Jonathan", grade: 80 },
-  { name: "Cindy", grade: 50 },
+  { name: "Edward", grade: 90, id: "1" },
+  { name: "Jonathan", grade: 80, id: "2" },
+  { name: "Cindy", grade: 50, id: "3" },
 ];
 
 // Student view of the assignment
 function ClassListView() {
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState("");
+  async function dropStudent(id: string) {
+    return fetch(`http://localhost:8080/class/${ClassId}/drop`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: id,
+      }),
+    })
+      .then((response) => {
+        if (response.status == 401) throw "Unauthorized";
+        return response.json();
+      })
+      .catch((e) => {
+        setError(
+          `Failed dropping student! Server responded with: ${String(e).replace(
+            "TypeError: ",
+            ""
+          )}`
+        );
+      });
+  }
+  async function handleDropStudent(id: string) {
+    await dropStudent(id);
+    handleClose();
+  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Container>
+      {error && <Alert variant={"danger"}>Failed to login: {error}</Alert>}
       <h1>My Students</h1>
       <Table striped bordered hover>
         <thead>
@@ -29,7 +64,29 @@ function ClassListView() {
               <td>{x.name}</td>
               <td>{x.grade}</td>
               <td>
-                <Button>Drop Student</Button>
+                <>
+                  <Button variant="primary" onClick={handleShow}>
+                    Drop Student
+                  </Button>
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Body>
+                      Are you sure you want to drop {x.name}?
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Cancel
+                      </Button>
+                      <Button
+                        variant="primary"
+                        onClick={() => {
+                          handleDropStudent(x.id);
+                        }}
+                      >
+                        Drop Student
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </>
               </td>
             </tr>
           ))}
