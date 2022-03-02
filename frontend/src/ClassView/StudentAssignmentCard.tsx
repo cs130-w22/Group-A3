e26d66fca7eb673b49df09637bac45aedd32b5aa
Jonathan Davies <jonathandaviesipad@gmail.com>
2022-03-01 22:45:37 -0800

    Style changes for assignment cards

--- frontend/src/ClassView/StudentAssignmentCard.tsx
+++ frontend/src/ClassView/StudentAssignmentCard.tsx
@@ -7,6 +7,10 @@ import Card from "react-bootstrap/Card";
 import ProgressBar from "react-bootstrap/ProgressBar";
 
 import { Link } from "react-router-dom";
+import {
+  buildStyles,
+  CircularProgressbarWithChildren,
+} from "react-circular-progressbar";
 
 // A card representing a single assignment overview
 function StudentAssignmentCard(props: { name: string }) {
@@ -18,17 +22,52 @@ function StudentAssignmentCard(props: { name: string }) {
       to="/class/assignment"
       style={{ textDecoration: "none", color: "inherit" }}
     >
-      <Card /* style={{ width: '18rem' }} */>
+      <Card style={{ borderRadius: 100, backgroundColor: "#f5f5f5" }}>
         <Card.Body>
-          <Card.Title>{props.name}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
+          <Card.Title
+            style={{ marginLeft: "4%", fontSize: 35, fontWeight: "bold" }}
+          >
+            {props.name}
+          </Card.Title>
+          <Card.Subtitle
+            style={{ marginLeft: "4%", fontSize: 18, fontWeight: "bolder" }}
+            className="mb-2 text-muted"
+          >
             {`Due: ${duedate}`}
           </Card.Subtitle>
           <Container>
             <Row>
-              <Col>Grade: </Col>
               <Col xs={11}>
-                <ProgressBar now={grade} label={`${grade}%`} />
+                <div
+                  className="row"
+                  style={{
+                    width: 140,
+                    height: 140,
+                    marginLeft: "95%",
+                    marginTop: "-7%",
+                    marginBottom: "-2%",
+                  }}
+                >
+                  <CircularProgressbarWithChildren
+                    value={grade}
+                    styles={buildStyles({
+                      pathColor: "#1273de",
+                      strokeLinecap: 1,
+                    })}
+                  >
+                    {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+                    <div
+                      style={{ fontSize: 20, marginTop: -15, color: "#808080" }}
+                    >
+                      <strong>Grade:</strong>
+                    </div>
+                    <div
+                      style={{ fontSize: 30, marginTop: -5, color: "#1273de" }}
+                    >
+                      <strong>{grade}%</strong>
+                    </div>
+                  </CircularProgressbarWithChildren>
+                </div>
               </Col>
             </Row>
           </Container>
