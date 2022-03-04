0c7e2788660301dcb28ab3940ed112832647ec75
Cindy Zheng <41048616+cindyrzheng@users.noreply.github.com>
2022-03-03 17:10:21 -0800

    Update frontend/src/AssignmentView/AddAssignmentModal.tsx
    Co-authored-by: leo <leo@krashanoff.com>

--- frontend/src/AssignmentView/AddAssignmentModal.tsx
+++ frontend/src/AssignmentView/AddAssignmentModal.tsx
@@ -39,7 +39,7 @@ function AddAssignmentModal() {
       }
     )
       .then((response) => {
-        if (response.status == 401) throw "Unauthorized";
+        if (response.status === 401) throw new Error("Unauthorized");
         return response.json();
       })
       .catch((e) => {
