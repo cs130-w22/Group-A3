35b1805b84f75b26f71d93375efc53079972eae4
krashanoff <leo@krashanoff.com>
2022-03-06 13:59:47 -0800

    Fix formatting

--- frontend/src/Modal/CreateClassModal.test.tsx
+++ frontend/src/Modal/CreateClassModal.test.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import CreateClassModal from './CreateClassModal';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import CreateClassModal from "./CreateClassModal";
 
 test("Fires onSubmit on button click", () => {
   let mockCalled = false;
-  render(<CreateClassModal show={true} loading={false} onHide={() => null} onSubmit={() => (mockCalled = true)} />);
-  
+
+  render(
+    <CreateClassModal
+      show={true}
+      loading={false}
+      onHide={() => null}
+      onSubmit={() => (mockCalled = true)}
+    />
+  );
+
   const button = screen.getByText("Submit");
   expect(button).toBeInTheDocument();
   button.click();
